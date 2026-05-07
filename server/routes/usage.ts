8fa1d837bd096f826c80510c8e2d2b442bad1c25
Charles Bennett <bleudebennett@proton.me>
2026-05-07 05:10:52 +0200

    fix: remove unnecessary usage route

--- server/routes/usage.ts
+++
@@ -1,9 +0,0 @@
-import { Hono } from "hono";
-
-const app = new Hono();
-
-app.get("/", async (c) => {
-  // Retrieve usage data, such as:
-  //  * Token usage last n days (independant calculation, not fetched from provider)
-  //  * Credits remaining (fetched from provider - if possible)
-});
