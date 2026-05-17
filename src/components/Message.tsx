cd5effe63ecbec5a2c8000cc1a3eaaf81f14a687
Charles Bennett <bleudebennett@proton.me>
2026-05-17 16:10:05 +0200

    refactor(css/component): updated some old stuff

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -3,17 +3,19 @@ import {
   CircleAlert,
   Copy,
   Pencil,
-  ThumbsDown,
-  ThumbsUp,
+  CircleDashed,
+  Star,
+  Check,
 } from "lucide-react";
 import type { Message } from "@server/schemas/message";
 import { useState } from "react";
 import Feedback from "./modal/Feedback";
 import { client } from "@/lib/client";
 import { useChatStore } from "@/store/chat";
-import Tooltip from "./Tooltip";
 import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 import CodeBlock from "./CodeBlock";
+import SmallButton from "./button/SmallButton";
 
 const Message = ({
   data,
@@ -61,17 +63,17 @@ const Message = ({
       {data.role === "user" ? (
         <div className="flex flex-col items-end gap-4 w-full group">
           {editMode ? (
-            <div className="flex flex-col gap-3 p-3 w-full bg-light-surface rounded-3xl border border-light-border">
+            <div className="flex flex-col gap-3 p-3 w-full bg-surface rounded-3xl border border-border">
               <div>
                 <textarea
                   onChange={(e) => setEditContent(e.target.value)}
                   value={editContent}
                   placeholder="Better get it right this time"
-                  className="flex px-3.5 py-2 w-full bg-light-surface-hover text-[15px] font-light rounded-2xl outline-none border border-light-border resize-none field-sizing-content placeholder:text-light-text-secondary"
+                  className="flex px-3.5 py-2 w-full bg-surface-hover text-[15px] font-light rounded-2xl outline-none border border-border resize-none field-sizing-content placeholder:text-text-secondary"
                 />
               </div>
               <div className="flex justify-between gap-4">
-                <div className="flex gap-2 text-light-text-secondary">
+                <div className="flex gap-2 text-text-secondary">
                   <CircleAlert className="size-5" />
                   <span className="flex text-[13px]">
                     Editing this message will create a new conversation branch.
@@ -82,42 +84,74 @@ const Message = ({
                 <div className="flex gap-2">
                   <button
                     onClick={() => setEditMode(false)}
-                    className="px-5 py-1.5 bg-light-surface rounded-2xl border border-light-border cursor-pointer hover:bg-light-surface-hover transition-colors"
+                    className="px-5 py-1.5 bg-surface rounded-2xl border border-border cursor-pointer hover:bg-surface-hover transition-colors"
                   >
                     <span className="text-[15px]">Cancel</span>
                   </button>
-                  <button className="px-5 py-1.5 bg-light-text-primary rounded-2xl cursor-pointer">
-                    <span className="text-[15px] text-light-bg">Submit</span>
+                  <button className="px-5 py-1.5 bg-text-primary rounded-2xl cursor-pointer">
+                    <span className="text-[15px] text-bg">Submit</span>
                   </button>
                 </div>
               </div>
             </div>
           ) : (
             <>
-              <div className="px-4 py-2 bg-light-surface rounded-3xl border border-light-border">
-                <span className="font-light">{data.content}</span>
+              <div className="px-4 py-2 max-w-full bg-surface rounded-3xl border border-border overflow-x-auto">
+                <span className="font-light">
+                  <ReactMarkdown
+                    remarkPlugins={[remarkGfm]}
+                    components={{
+                      code({ className, children }) {
+                        const language = /language-(\w+)/.exec(
+                          className || "",
+                        )?.[1];
+                        return (
+                          <CodeBlock language={language}>
+                            {String(children)}
+                          </CodeBlock>
+                        );
+                      },
+                    }}
+                  >
+                    {data.content}
+                  </ReactMarkdown>
+                </span>
               </div>
               <div className="flex justify-end items-center gap-2 opacity-0 group-hover:opacity-100 transition-opacity">
-                <span className="text-sm text-light-text-secondary">
+                <span className="text-sm text-text-secondary">
                   {new Date(data.createdAt).toLocaleTimeString()}
                 </span>
-                <button
+                <SmallButton
+                  className="hover:bg-bg-hover group/text-hover"
                   onClick={() => {
                     setEditContent(data.content);
                     setEditMode(true);
                   }}
-                  className="relative p-1.5 rounded-full hover:bg-light-bg-hover transition-colors group/text-hover group/tooltip"
-                >
-                  <Pencil className="size-4.25 text-light-text-secondary group-hover/text-hover:text-light-text-primary" />
-                  <Tooltip label="Edit" position="top" />
-                </button>
-                <button
-                  onClick={() => navigator.clipboard.writeText(data.content)}
-                  className="relative p-1.5 rounded-full hover:bg-light-bg-hover transition-colors group/text-hover group/tooltip"
-                >
-                  <Copy className="size-4.25 text-light-text-secondary group-hover/text-hover:text-light-text-primary" />
-                  <Tooltip label="Copy" position="top" />
-                </button>
+                  icon={
+                    <Pencil className="size-4.25 text-text-secondary group-hover/text-hover:text-text-primary" />
+                  }
+                  label={{ content: "Edit", tooltip: true, position: "top" }}
+                />
+                <SmallButton
+                  className="hover:bg-bg-hover group/text-hover"
+                  onClick={() => {
+                    navigator.clipboard.writeText(data.content);
+                    setCopied(true);
+                    setTimeout(() => setCopied(false), 2000);
+                  }}
+                  icon={
+                    copied ? (
+                      <Check className="size-4.25 text-text-secondary group-hover/text-hover:text-text-primary" />
+                    ) : (
+                      <Copy className="size-4.25 text-text-secondary group-hover/text-hover:text-text-primary" />
+                    )
+                  }
+                  label={{
+                    content: copied ? "Copied" : "Copy",
+                    tooltip: true,
+                    position: "top",
+                  }}
+                />
               </div>
             </>
           )}
@@ -144,6 +178,29 @@ const Message = ({
                     </CodeBlock>
                   );
                 },
+                table({ children }) {
+                  return (
+                    <div className="my-4 overflow-x-auto">
+                      <table className="w-full text-sm border-collapse">
+                        {children}
+                      </table>
+                    </div>
+                  );
+                },
+                th({ children }) {
+                  return (
+                    <th className="px-4 py-2 bg-surface text-left font-normal border border-border">
+                      {children}
+                    </th>
+                  );
+                },
+                td({ children }) {
+                  return (
+                    <td className="border border-border px-4 py-2">
+                      {children}
+                    </td>
+                  );
+                },
               }}
             >
               {data.content}
@@ -155,27 +212,53 @@ const Message = ({
               ${!isLast && "group-hover:opacity-100"}
             `}
           >
-            <button
-              onClick={() => navigator.clipboard.writeText(data.content)}
-              className="relative p-1.5 rounded-full  hover:bg-light-bg-hover transition-colors group/text-hover group/tooltip"
-            >
-              <Copy className="size-4.25 text-light-text-secondary group-hover/text-hover:text-light-text-primary" />
-              <Tooltip label="Copy" position="top" />
-            </button>
-            <button
-              onClick={() => setFeedbackModal("positive")}
-              className="relative p-1.5 rounded-full hover:bg-light-bg-hover transition-colors group/text-hover group/tooltip"
-            >
-              <ThumbsUp className="size-4.25 text-light-text-secondary group-hover/text-hover:text-light-text-primary" />
-              <Tooltip label="Good response" position="top" />
-            </button>
-            <button
-              onClick={() => setFeedbackModal("negative")}
-              className="relative p-1.5 rounded-full hover:bg-light-bg-hover transition-colors group/text-hover group/tooltip"
-            >
-              <ThumbsDown className="size-4.25 text-light-text-secondary group-hover/text-hover:text-light-text-primary" />
-              <Tooltip label="Bad response" position="top" />
-            </button>
+            <SmallButton
+              className="hover:bg-bg-hover group/text-hover"
+              onClick={() => {
+                navigator.clipboard.writeText(data.content);
+                setCopied(true);
+                setTimeout(() => setCopied(false), 2000);
+              }}
+              icon={
+                copied ? (
+                  <Check className="size-4.25 text-text-secondary group-hover/text-hover:text-text-primary" />
+                ) : (
+                  <Copy className="size-4.25 text-text-secondary group-hover/text-hover:text-text-primary" />
+                )
+              }
+              label={{
+                content: copied ? "Copied" : "Copy",
+                tooltip: true,
+                position: "top",
+              }}
+            />
+            {data.feedback ? (
+              <SmallButton
+                onClick={() => setFeedbackModal(true)}
+                className="hover:bg-bg-hover group/text-hover"
+                icon={
+                  <Star className="size-4.25 text-text-secondary group-hover/text-hover:text-text-primary" />
+                }
+                label={{
+                  content: "Update feedback",
+                  tooltip: true,
+                  position: "top",
+                }}
+              />
+            ) : (
+              <SmallButton
+                onClick={() => setFeedbackModal(true)}
+                className="hover:bg-bg-hover group/text-hover"
+                icon={
+                  <CircleDashed className="size-4.25 text-text-secondary group-hover/text-hover:text-text-primary" />
+                }
+                label={{
+                  content: "Give feedback",
+                  tooltip: true,
+                  position: "top",
+                }}
+              />
+            )}
             <span className="pl-1 text-sm text-light-text-secondary">
               {data.duration}s ·{" "}
               {(data.completionTokens! / data.duration!).toFixed(2)} tok/s ·{" "}
